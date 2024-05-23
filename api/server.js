fc45fdf11e19d08ce78050154cdbb6e68295a3df
bersitedi <bersitedi421@gmail.com>
2024-05-23 22:48:43 +0200

    initialize both our server and frontend

---
+++ api/server.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
